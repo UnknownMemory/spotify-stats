6574b06a360a58f2c8249b8cce5b284354b4ea06
Jérémy <jeremyschneider21@gmail.com>
2020-02-11 20:56:12 +0100

    Add React

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,21 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import APP_ROOT from 'app-root-path';
 
-require('dotenv').config()
+require('dotenv').config();
+
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-})
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+app.use(express.static(APP_ROOT + '/dist/client'));
 
-app.listen(process.env.PORT, function () {
-    console.log(`Express server started at http://localhost:${process.env.PORT}`)
-})
+app.listen(process.env.PORT, function() {
+    console.log(
+        `Express server started at http://localhost:${process.env.PORT}`
+    );
+});
